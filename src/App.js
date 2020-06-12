405ad654ce7721bdb97f7cc254e66f019905e6b6
Volodymyr Gorbunov <deftime@gmail.com>
2020-06-12 14:45:49 +0300

    Ls 1-7

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <Tehno />
     </div>
   );
+}
+
+function Header() {
+  return (
+    <div className="menu">
+      <a href="#">Main</a><br />
+      <a href="#">Services</a><br />
+      <a href="#">Contacts</a>
+    </div>
+  );
+}
+
+const Tehno = () => {
+  return (
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JS</li>
+    </ul>
+  )
 }
 
 export default App;

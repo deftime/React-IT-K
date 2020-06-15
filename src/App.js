09111b8450b68c338c8c1e33d12e46c689b42bff
Volodymyr Gorbunov <deftime@gmail.com>
2020-06-15 19:20:49 +0300

    Ls 8-10

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './pages/header';
+import Tehno from './pages/tehno';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App App-header">
+    <img src={logo} width="300"/>
     <Header />
     <Tehno />
     </div>
   );
-}
-
-function Header() {
-  return (
-    <div className="menu">
-      <a href="#">Main</a><br />
-      <a href="#">Services</a><br />
-      <a href="#">Contacts</a>
-    </div>
-  );
-}
-
-const Tehno = () => {
-  return (
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JS</li>
-    </ul>
-  )
 }
 
 export default App;
---
+++ src/pages/tehno.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Tehno = () => {
+  return (
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JS</li>
+    </ul>
+  )
+}
+
+export default Tehno;

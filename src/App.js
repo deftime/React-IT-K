bc41c0e9040042edca8196afe16d0db34400dbda
Volodymyr Gorbunov <deftime@gmail.com>
2020-06-17 16:57:13 +0300

    Ls 11 layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './pages/header';
-import Tehno from './pages/tehno';
+import logolink from './images/logo.png';
 
 function App() {
   return (
-    <div className="App App-header">
-    <img src={logo} width="300"/>
-    <Header />
-    <Tehno />
+    <div className="App">
+      <div className="wrapper">
+        <header>
+          <img src={logolink} alt="main_logo" width="60" />
+        </header>
+        <aside>
+          <a href="#">Profile</a>
+          <a href="#">Messages</a>
+          <a href="#">News</a>
+          <a href="#">Music</a>
+          <a href="#">Settings</a>
+        </aside>
+        <div className="main">
+          <div className="user-cover main-row"></div>
+          <div className="user-data main-row">
+            <div className="user-ava"></div>
+            <div className="user-details">
+              <span>[USER_NAME]</span><br />
+              <span>Age:</span><br />
+              <span>Country:</span><br />
+              <span>City:</span><br />
+              <span>Education:</span><br />
+              <span>Web:</span><br />
+            </div>
+          </div>
+          <div className="message-box main-row">
+            <div className="message-box-wrap">
+              <div>New Post</div>
+              <form className="send-msg">
+                <textarea name="post_msg"></textarea><br />
+                <button type="submit" name="send_msg">Send</button>
+              </form>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
